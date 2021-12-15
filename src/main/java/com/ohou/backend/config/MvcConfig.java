2723f7c9d5f56824362a915c86200ae1479f9464
suky0713@kyonngi.ac.kr <suky0713@kyonggi.ac.kr>
2021-12-15 13:53:54 +0900

    no message

--- src/main/java/com/ohou/backend/config/MvcConfig.java
+++ src/main/java/com/ohou/backend/config/MvcConfig.java
@@ -10,6 +10,6 @@ class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000,http://localhost:8080");
     }
 }
